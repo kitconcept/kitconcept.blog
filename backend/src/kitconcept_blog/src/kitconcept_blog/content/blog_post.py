9c1b3d143e20bb25f9833e9ade4cc65c61dc9904
jonaspiterek <piterek@kitconcept.com>
2023-11-07 19:39:12 +0100

    add blog post content type

---
+++ backend/src/kitconcept_blog/src/kitconcept_blog/content/blog_post.py
@@ -0,0 +1,48 @@
+from kitconcept_blog import _
+from plone.dexterity.content import Container
+from plone.supermodel.model import Schema
+from zope import schema
+from zope.interface import implementer
+
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
+from plone.autoform import directives
+
+
+class IBlogPost(Schema):
+    """A Blog post."""
+
+    title = schema.TextLine(
+        title=_('Title'),
+        description=_('Blog post title'),
+        required=True,
+    )
+
+    description = schema.TextLine(
+        title=_('Description'),
+        description=_('Blog post description'),
+        required=False,
+    )
+
+    authors = RelationList(
+        title=_('Authors'),
+        description=_('Blog post authors'),
+        required=False,
+        value_type=RelationChoice(
+            vocabulary="plone.app.vocabularies.Catalog",
+        ),
+    )
+    directives.widget(
+        "authors",
+        frontendOptions={
+            "widget": "object_browser",
+            "widgetProps": {
+                "selectableTypes": ["Author"],
+            },
+        },
+    )
+
+
+@implementer(IBlogPost)
+class BlogPost(Container):
+    """A Blog post."""
