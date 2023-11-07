f1292158b9a1056321580737641d0f3f2014d66b
jonaspiterek <piterek@kitconcept.com>
2023-11-07 19:43:34 +0100

    make format

--- backend/src/kitconcept_blog/src/kitconcept_blog/content/blog_post.py
+++ backend/src/kitconcept_blog/src/kitconcept_blog/content/blog_post.py
@@ -1,32 +1,31 @@
 from kitconcept_blog import _
+from plone.autoform import directives
 from plone.dexterity.content import Container
 from plone.supermodel.model import Schema
-from zope import schema
-from zope.interface import implementer
-
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
-from plone.autoform import directives
+from zope import schema
+from zope.interface import implementer
 
 
 class IBlogPost(Schema):
     """A Blog post."""
 
     title = schema.TextLine(
-        title=_('Title'),
-        description=_('Blog post title'),
+        title=_("Title"),
+        description=_("Blog post title"),
         required=True,
     )
 
     description = schema.TextLine(
-        title=_('Description'),
-        description=_('Blog post description'),
+        title=_("Description"),
+        description=_("Blog post description"),
         required=False,
     )
 
     authors = RelationList(
-        title=_('Authors'),
-        description=_('Blog post authors'),
+        title=_("Authors"),
+        description=_("Blog post authors"),
         required=False,
         value_type=RelationChoice(
             vocabulary="plone.app.vocabularies.Catalog",
